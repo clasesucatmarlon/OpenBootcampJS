5a70a670e398be6f794bb30be6ff4134fb753550
clasesucatmarlon <clasesucatmarlon@gmail.com>
2022-05-26 22:03:52 -0500

    Entrega Ejercicio 1 - Módulo 02

---
+++ Sesion_02/index.js
@@ -0,0 +1,12 @@
+const array = [
+  'Marlon García',
+  49,
+  true,
+  new Date('11/16/1972'),
+  {
+    'titulo': '100 años de soledad',
+    'author': 'Romulo Gallegos',
+    'fecha': new Date('3/5/67'),
+    'url': 'http://www.secst.cl/upfiles/documentos/19072016_1207am_578dc39115fe9.pdf'
+  }
+]
